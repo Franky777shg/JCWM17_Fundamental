1ed9f1041fbc51bdef73dd2f7f63cbd628dada0e
Franky777shg <frengky.sihombing.777@gmail.com>
2021-04-26 12:03:05 +0700

    Running code, console.log, and comment

---
+++ Running_Code.js
@@ -0,0 +1,25 @@
+// Comment itu adalah catatan atau tulisan di code yang tidak akan di execute
+// short cut untuk comment adalah ctrl + "/"
+
+// Comment itu berfungsi sebagai berikut:
+// 1. Catatan per code section
+// example:
+// ini algoritma untuk page login
+
+// 2. Catatan komunikasi pribadi atau antar team
+
+// Comment untuk satu line, bisa pakai double slash / "//"
+// example: 
+// Ini Comment
+// Ini comment 2
+
+// Comment untuk lebih dari satu line, bisa pakai "/**/"
+// example:
+
+/* Ini
+untuk
+comment lebih dari
+satu line*/
+
+// ini code untuk console.log
+console.log('Hello World')
