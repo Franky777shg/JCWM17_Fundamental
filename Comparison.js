b4017ef837696b094e9ab08f5da7c7d0c394fd82
Franky777shg <frengky.sihombing.777@gmail.com>
2021-04-28 09:47:41 +0700

    boolean and conditional

---
+++ Comparison.js
@@ -0,0 +1,15 @@
+// Boolean
+console.log(5 > 2) 
+console.log(5 < 2)
+console.log(5 >= 5)
+console.log(5 <= 5)
+console.log(5 == "5") // == => membandingkan value nya saja
+console.log(5 === "5") // === => membandingkan value beserta tipe data nya
+console.log(5 != 5)
+console.log(5 !== "5")
+console.log(1 < 2 && false)
+console.log(1 < 2 || false)
+console.log(!true)
+console.log(null == undefined)
+console.log(null === undefined)
+
---
+++ SyntaxCondition.js
@@ -0,0 +1,20 @@
+// IF
+let age = 18
+if (age >= 18) console.log('SIM akan segera dibuat')
+
+// IF ELSE
+if (age >= 18) {
+    console.log('SIM akan segera dibuat')
+} else {
+    console.log('Permohonan SIM ditolak')
+}
+
+// IF ELSE IF ELSE
+let weight = 55
+if (weight < 50) {
+    console.log('Mohon tingkatkan berat badan Anda')
+} else if (weight >= 50 && weight <= 60) {
+    console.log('Selamat! Kamu diterima')
+} else {
+    console.log('Mohon kurangi berat badan Anda')
+}
