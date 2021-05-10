33b499a45e69fb5b553c0716f98229dfbe7e601f
Franky777shg <frengky.sihombing.777@gmail.com>
2021-05-10 09:38:54 +0700

    jawaban quiz

---
+++ ageRatio.js
@@ -0,0 +1,8 @@
+let totalAge = 49,
+    x = 4,
+    y = 10,
+    totalRatio = x + y,
+    Andi = x * (totalAge / totalRatio),
+    Budi = y * (totalAge / totalRatio)
+
+console.log(`Umur Andi = ${Andi} dan Budi = ${Budi}`)
---
+++ crashCar.js
@@ -0,0 +1,8 @@
+let jarak = 120,
+    speedA = 60,
+    speedB = 40,
+    startTime = 9,
+    crashTime = (jarak / (speedA + speedB))
+
+
+console.log(crashTime)
