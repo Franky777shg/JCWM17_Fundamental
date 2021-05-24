ec8d1536496e0b631df45bafcd004b906225e02c
Franky777shg <frengky.sihombing.777@gmail.com>
2021-05-24 10:47:17 +0700

    update input data

---
+++ database.js
@@ -0,0 +1,40 @@
+let products = [
+    {
+        id: 1621812363864,
+        kate: "Fast Food",
+        nama: "Noodle",
+        harga: 3500,
+        stok: 9
+    },
+    {
+        id: 1621890331864,
+        kate: "Electronic",
+        nama: "Headset",
+        harga: 300000,
+        stok: 8
+    },
+    {
+        id: 1621889363823,
+        kate: "Cloth",
+        nama: "Hoodie",
+        harga: 150000,
+        stok: 7
+    },
+    {
+        id: 1621872343860,
+        kate: "Fruit",
+        nama: "Apple",
+        harga: 10000,
+        stok: 8
+    }
+]
+
+class Product {
+    constructor(id, kate, nama, harga, stok){
+        this.id = id
+        this.kate = kate
+        this.nama = nama
+        this.harga = harga
+        this.stok = stok
+    }
+}
